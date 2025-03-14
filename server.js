961a6431e6c64173f2b071992008b8102dce97da
Rook <98975295+RunesofYggdrasil@users.noreply.github.com>
2025-03-14 15:24:36 -0400

    Fixed server.ts to server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const next = require("next");
+const http = require("http");
+const url = require("url");
+
+const port = parseInt(process.env.PORT || "3001", 10);
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  http
+    .createServer((req, res) => {
+      // if (req.url.startsWith("/api")) {
+      //   // Your API handling logic here
+      // } else {
+      //   // Handle Next.js routes
+      //   return app.getRequestHandler()(req, res);
+      // }
+      const parsedUrl = url.parse(req.url, true);
+      handle(req, res, parsedUrl);
+    })
+    .listen(port);
+});
