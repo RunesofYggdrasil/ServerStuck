30a83b1030b777aed23be164c7d55c7da5ce307e
Rook <98975295+RunesofYggdrasil@users.noreply.github.com>
2025-03-14 15:54:58 -0400

    Port Change

--- Dockerfile
+++ Dockerfile
@@ -43,9 +43,9 @@ COPY --from=builder --chown=nextjs:nodejs /app/.next/static ./.next/static
 
 USER nextjs
 
-EXPOSE 3001
+EXPOSE 3000
 
-ENV PORT=3001
+ENV PORT=3000
 
 # RUN npm run build
 # CMD ["node", "server.ts"]
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const next = require("next");
 const http = require("http");
 const url = require("url");
 
-const port = parseInt(process.env.PORT || "3001", 10);
+const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
